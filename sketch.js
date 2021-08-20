cb2cf30c2447da7369859066e98ed29dd26be85c
Ruby Adkins <radkins22@students.stab.org>
2021-08-20 02:57:19 -0400

    add layers + texture to cake

--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
-let cake_lining_texture;
+let cake_lining_texture, chocolate_texture, strawberry_texture, vanilla_texture;
+
 function preload() {
   cake_lining_texture = loadImage('images/candycane.jpg');
+  chocolate_texture = loadImage('images/chocolatecake.png');
+  strawberry_texture = loadImage('images/strawberry_texture.jpg');
+  vanilla_texture = loadImage('images/vanilla_texture.jpg');
 }
 
 function setup() {
@@ -19,24 +23,71 @@ function draw() {
   //cake base layer
   push();
   noStroke();
-  rotateY(frameCount * 0.01);
-  cylinder(160, 100);
+  translate(0, 30);
+  ambientMaterial(160,82,45);
+  cylinder(160, 35);
+  pop();
+
+  push();
+  noStroke();
+  translate(0, -30);
+  ambientMaterial(160,82,45);
+  cylinder(160, 35);
+  pop();
+
+  push();
+  noStroke();
+  ambientMaterial(60,19,33);
+  texture(chocolate_texture)
+  cylinder(160, 30);
   pop();
   
   //cake second layer
   push();
   noStroke();
+  translate(0, -70);
+  ambientMaterial(160,82,45);
+  cylinder(100, 35);
+  pop();
+
+  push();
+  noStroke();
+  translate(0, -130);
+  ambientMaterial(160,82,45);
+  cylinder(100, 35);
+  pop();
+
+  push();
+  noStroke();
   translate(0, -100);
-  rotateY(frameCount * 0.01);
-  cylinder(100, 100);
+  ambientMaterial(255, 243, 196);
+  texture(vanilla_texture);
+  cylinder(100, 30);
   pop();
   
   //cake top layer
   push();
   noStroke();
-  translate(0, -180);
+  translate(0, -210);
   rotateY(frameCount * 0.01);
-  cylinder(50, 80);
+  ambientMaterial(160,82,45);
+  cylinder(50, 30);
+  pop();
+
+  push();
+  noStroke();
+  translate(0, -160);
+  rotateY(frameCount * 0.01);
+  ambientMaterial(160,82,45);
+  cylinder(50, 30);
+  pop();
+
+  push();
+  noStroke();
+  translate(0, -185);
+  ambientMaterial(255, 196, 226);
+  texture(strawberry_texture);
+  cylinder(50, 20);
   pop();
   
   //cake candles
